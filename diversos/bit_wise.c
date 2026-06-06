47943e45a96444c745f94321081f6594bd18ff37
Dácio Braga <fdacio@gmail.com>
2026-06-06 00:49:51 -0300

    feat: ajuste

--- diversos/bit_wise.c
+++ diversos/bit_wise.c
@@ -2,18 +2,25 @@
 #include <stdlib.h>
 
 #define BIT_0 (1 << 0) // bit 0
+#define BIT_1 (1 << 1) // bit 1
+#define BIT_2 (1 << 2) // bit 2
 #define BIT_3 (1 << 3) // bit 3
 #define BIT_4 (1 << 4) // bit 4
 #define BIT_5 (1 << 5) // bit 5
+#define BIT_6 (1 << 6) // bit 6
+#define BIT_7 (1 << 7) // bit 7
 
+//value = 00000010 e bit_position = 0 -> (value >> bit_position) = 00000010 -> 00000010 & 00000001 = 0
+//value = 00000010 e bit_position = 1 -> (value >> bit_position) = 00000001 -> 00000001 & 00000001 = 1
+//value = 00000010 e bit_position = 2 -> (value >> bit_position) = 00000000 -> 00000000 & 00000001 = 0
 int read_bit(int value, int bit_position) {
   // Desloca o bit para a direita e isola com o operador AND (&)
-  return (value >> bit_position) & 1;
+  return (value >> bit_position) & 0b00000001;
 }
 
 int read_bit_char(unsigned char value, int bit_position) {
   // Desloca o bit para a direita e isola com o operador AND (&)
-  return (value >> bit_position) & 1;
+  return (value >> bit_position) & 0b00000001;
 }
 
 void byte_to_binary(unsigned char byte, char *result) {
@@ -53,10 +60,8 @@ int main() {
   char ch2 = 65;
   unsigned char ch3 = 0b00000010;
 
-  printf("%8d %25s %s\n", value1, "(unsigned char) in binary",
-         to_binary(value1));
-  printf("%8d %25s %s\n", value4, "(unsigned char) in binary",
-         to_binary(value4));
+  printf("%8d %25s %s\n", value1, "(unsigned char) in binary", to_binary(value1));
+  printf("%8d %25s %s\n", value4, "(unsigned char) in binary", to_binary(value4));
 
   printf("%8d %25s %s\n", value2, "(signed char) in binary", to_binary(value2));
   printf("%8d %25s %s\n", value3, "(signed char) in binary", to_binary(value3));
@@ -67,7 +72,6 @@ int main() {
   printf("%8d %25s %s\n", ch3, "(unsigned char) in binary", to_binary(ch3));
   printf("\n");
 
-
   printf("(1<<0) - %s - %d\n", to_binary(1 << 0), (1 << 0));
   printf("(1<<1) - %s - %d\n", to_binary(1 << 1), (1 << 1));
   printf("(1<<2) - %s - %d\n", to_binary(1 << 2), (1 << 2));
@@ -76,25 +80,35 @@ int main() {
   printf("(1<<5) - %s - %d\n", to_binary(1 << 5), (1 << 5));
   printf("(1<<6) - %s - %d\n", to_binary(1 << 6), (1 << 6));
   printf("(1<<7) - %s - %d\n", to_binary(1 << 7), (1 << 7));
+  printf("\n");
+
   printf("(2<<2) - %s - %d\n", to_binary(2 << 2), (2 << 2));
   printf("(3<<2) - %s - %d\n", to_binary(3 << 2), (3 << 2));
   printf("\n");
 
   printf("Leitura de bits usando shift e AND:\n");
   printf("-------------------------------------------------\n");
-  
+
   unsigned char value = 47; // 0 a 255
   char *binary_1 = "0b00000001";
 
   printf("%8d in binary %s\n", value, to_binary(value));
-  printf("(%d>>0): %3d - %s & %s = %d\n", value, (value >> 0), to_binary((value >> 0)), binary_1, (value >> 0) & 1);
-  printf("(%d>>1): %3d - %s & %s = %d\n", value, (value >> 1), to_binary((value >> 1)), binary_1, (value >> 1) & 1);
-  printf("(%d>>2): %3d - %s & %s = %d\n", value, (value >> 2), to_binary((value >> 2)), binary_1, (value >> 2) & 1);
-  printf("(%d>>3): %3d - %s & %s = %d\n", value, (value >> 3), to_binary((value >> 3)), binary_1, (value >> 3) & 1);
-  printf("(%d>>4): %3d - %s & %s = %d\n", value, (value >> 4), to_binary((value >> 4)), binary_1, (value >> 4) & 1);
-  printf("(%d>>5): %3d - %s & %s = %d\n", value, (value >> 5), to_binary((value >> 5)), binary_1, (value >> 5) & 1);
-  printf("(%d>>6): %3d - %s & %s = %d\n", value, (value >> 6), to_binary((value >> 6)), binary_1, (value >> 6) & 1);
-  printf("(%d>>7): %3d - %s & %s = %d\n", value, (value >> 7), to_binary((value >> 7)), binary_1, (value >> 7) & 1);
+  printf("(%d>>0): %3d - %s & %s = %d\n", value, (value >> 0),
+         to_binary((value >> 0)), binary_1, (value >> 0) & 1);
+  printf("(%d>>1): %3d - %s & %s = %d\n", value, (value >> 1),
+         to_binary((value >> 1)), binary_1, (value >> 1) & 1);
+  printf("(%d>>2): %3d - %s & %s = %d\n", value, (value >> 2),
+         to_binary((value >> 2)), binary_1, (value >> 2) & 1);
+  printf("(%d>>3): %3d - %s & %s = %d\n", value, (value >> 3),
+         to_binary((value >> 3)), binary_1, (value >> 3) & 1);
+  printf("(%d>>4): %3d - %s & %s = %d\n", value, (value >> 4),
+         to_binary((value >> 4)), binary_1, (value >> 4) & 1);
+  printf("(%d>>5): %3d - %s & %s = %d\n", value, (value >> 5),
+         to_binary((value >> 5)), binary_1, (value >> 5) & 1);
+  printf("(%d>>6): %3d - %s & %s = %d\n", value, (value >> 6),
+         to_binary((value >> 6)), binary_1, (value >> 6) & 1);
+  printf("(%d>>7): %3d - %s & %s = %d\n", value, (value >> 7),
+         to_binary((value >> 7)), binary_1, (value >> 7) & 1);
   printf("\n");
 
   // unsigned char PORT1 =  0;
@@ -135,33 +149,39 @@ int main() {
 
   // BIT_0(1<<0) - liga bit 0 do PORT1
   printf("(PORT1 |= BIT_0)-> Ligar BIT_0 do PORT1\n");
-  PORT1 |= BIT_0; // Equivale a PORT1 = PORT1 | BIT_0, operação PORT1 = 0b00000000 | 0b00000001 -> PORT1 = 0b00000001
+  PORT1 |= BIT_0; // Equivale a PORT1 = PORT1 | BIT_0, operação PORT1 =
+                  // 0b00000000 | 0b00000001 -> PORT1 = 0b00000001
   byte_to_binary(PORT1, string_binaria);
   printf("Novo valor do PORT1: %s\n", string_binaria);
   printf("\n");
 
   // BIT_3(1<<3) - desliga bit 3 do PORT2
   printf("(PORT2 &= ~BIT_3)-> Desligar BIT_3 do PORT2\n");
-  PORT2 &= ~BIT_3; // Equivale a PORT2 = PORT2 & ~BIT_3, operação PORT2 = 0b00001010 & 0b11110111 -> PORT2 = 0b00000010
+  PORT2 &= ~BIT_3; // Equivale a PORT2 = PORT2 & ~BIT_3, operação PORT2 =
+                   // 0b00001010 & 0b11110111 -> PORT2 = 0b00000010
   byte_to_binary(PORT2, string_binaria);
   printf("Novo valor de PORT2: %s\n", string_binaria);
   printf("\n");
 
   // BIT_5(1<<5) - alterna o bit 5 do PORT3
   printf("(PORT3 ^= BIT_5)-> Toggle o estado do BIT_5 do PORT3\n");
-  PORT3 ^= BIT_5; // Equivale a PORT3 = PORT3 ^ BIT_5, operação PORT3 = 0b00100100 ^ 0b00100000 -> PORT3 = 0b00000100
+  PORT3 ^= BIT_5; // Equivale a PORT3 = PORT3 ^ BIT_5, operação PORT3 =
+                  // 0b00100100 ^ 0b00100000 -> PORT3 = 0b00000100
   byte_to_binary(PORT3, string_binaria);
   printf("Toggle 1 valor de PORT3: %s\n", string_binaria);
-  PORT3 ^= BIT_5; // Equivale a PORT3 = PORT3 ^ BIT_5, operação PORT3 = 0b00000100 ^ 0b00100000 -> PORT3 = 0b00100100
+  PORT3 ^= BIT_5; // Equivale a PORT3 = PORT3 ^ BIT_5, operação PORT3 =
+                  // 0b00000100 ^ 0b00100000 -> PORT3 = 0b00100100
   byte_to_binary(PORT3, string_binaria);
   printf("Toggle 2 valor de PORT3: %s\n", string_binaria);
-  PORT3 ^= BIT_5;// Equivale a PORT3 = PORT3 ^ BIT_5, operação PORT3 = 0b00100100 ^ 0b00100000 -> PORT3 = 0b00000100
+  PORT3 ^= BIT_5; // Equivale a PORT3 = PORT3 ^ BIT_5, operação PORT3 =
+                  // 0b00100100 ^ 0b00100000 -> PORT3 = 0b00000100
   byte_to_binary(PORT3, string_binaria);
   printf("Toggle 3 valor de PORT3: %s\n", string_binaria);
   printf("\n");
 
   printf("(PORT4 & BIT_4) -> Leitura do bit 4 de PORT4:\n");
-  int bit4_in_port4 = (PORT4 & BIT_4) ? 1 : 0; // Verifica se o bit 4 de PORT4 está ligado
+  int bit4_in_port4 =
+      (PORT4 & BIT_4) ? 1 : 0; // Verifica se o bit 4 de PORT4 está ligado
   if (bit4_in_port4) {
     printf("O bit 4 de PORT4 está ligado.\n");
   } else {
--- diversos/ponteiros.c
+++ diversos/ponteiros.c
@@ -1,76 +1,86 @@
+#include <ctype.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#include <ctype.h>
 
-int main()
-{
-    printf("Ponteiros para strings\n");
-    char *p1;
-    char text[80] = "C completo e total";
-    printf("%s\n", text);
-    printf("%p\n", text);
+int main() {
+
+  char c = 'A';
+  char *_pointer1 = &c;
+  char *_pointer2 = _pointer1 + 1;
+  printf("%p\n", (void *)&_pointer1);
+  printf("%p\n", (void *)&_pointer2);
+  printf("%p\n", (void *)_pointer1);
+  printf("%c\n", *_pointer1);
+
+  printf("Ponteiros para strings\n");
+  char *p1;
+  char text[80] = "C completo e total";
+  printf("%s\n", text);
+  printf("%p\n", (void *)text);
+
+  p1 = text;
+  printf("%p\n", (void *)p1);
+  printf("%p\n", (void *)p1);
+
+  printf("\n\nPonteiros para inteiros\n");
+  int *p2;
+  int numbers[5] = {1, 2, 3, 4, 5};
+
+  p2 = numbers;
+  while (*p2) {
+    printf("%d|%p - ", *p2, (void *)p2);
+    p2++;
+  }
+  printf("\n");
+
+  p2 = numbers;
+  printf("%d\n", *numbers);
+  printf("%p\n", (void *)numbers);
+  printf("%p\n", (void *)++p2);
+  printf("%p\n", (void *)++p2);
+
+  /*
+  char *text;
+  text = (char*) malloc(80 * sizeof(char));
+  if (!text) {
+      printf("Memory erro!");
+      return 1;
+  }
+  */
+  do {
     p1 = text;
-    printf("%p\n", ++p1);
-    printf("%p\n", ++p1);
+    printf("Informe uma palavra: ");
+    fgets(text, sizeof(text), stdin);
+    // scanf("%s", text);
 
-    printf("\n\nPonteiros para inteiros\n");
-    int *p2;
-    int numbers[5] = {1, 2, 3, 4, 5};
-
-    p2 = numbers;
-    while(*p2) {
-        printf("%d|%p - ", *p2, p2);
-        p2++;
+    printf("Upper Case -> ");
+    while (*p1) {
+      char _pipe = (p1 == text ? '|' : (char)0);
+      printf("%c%c|", _pipe, toupper(*p1++));
     }
     printf("\n");
 
-    p2 = numbers;
-    printf("%d\n",*numbers);
-    printf("%p\n", numbers);
-    printf("%p\n", ++p2);
-    printf("%p\n", ++p2);
-
-    /*
-    char *text;
-    text = (char*) malloc(80 * sizeof(char));
-    if (!text) {
-        printf("Memory erro!");
-        return 1;
+    p1 = text;
+    printf("ASCII -> ");
+    while (*p1) {
+      char _pipe = (p1 == text ? '|' : (char)0);
+      char c = *p1;
+      printf("%c%c:%d|", _pipe, c, c);
+      p1++;
     }
-    */
-    do
-    {
-        p1 = text;
-        printf("Informe uma palavra: ");
-        //gets(text);
-        scanf("%s", text);
+    printf("\n");
 
-        printf("Upper Case -> ");
-        while (*p1) {   
-            printf("%c%c|", (p1 - 1 == text ? '|' : (char)0), toupper(*p1++));
-        }
-        printf("\n");
-        
-        p1 = text;
-        printf("ASCII -> ");
-        while (*p1) {
-            char c = *p1;
-            printf("%c%c-%d|", (p1 == text) ? '|' : (char)0, c, c);
-            p1++;
-        }
-        printf("\n");
+    printf("Inverse -> ");
+    char _pipe = (p1 == text ? '|' : (char)0);
+    for (p1 = text + strlen(text) - 1; p1 >= text; p1--) {
+      printf("%c%c|", _pipe, *(p1));
+    }
+    printf("\n");
+    printf("--------------------------------------------------\n");
+    printf("text: %s\n", text);
 
-       
-        printf("Inverse -> ");
-        for (p1 = text + strlen(text) - 1 ; p1 >= text; p1--) {
-            printf("%c%c|", (p1 == text + strlen(text) - 1 ? '|' : (char)0), *(p1));
-        }
-        printf("\n");
+  } while (strcmp(text, "done\n") != 0 && strcmp(text, "done") != 0);
 
-    } while (strcmp(text, "done"));
-    
-    printf("Fim do programa\n");
-    
+  printf("Fim do programa\n");
 }
-
